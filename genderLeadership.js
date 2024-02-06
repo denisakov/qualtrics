f8fd6a8c3c58e8a16e757ad694a70b60eaa5c03e
Denis Isakov <denisakov@gmail.com>
2024-02-06 03:06:08 +0100

    ytuytuy

--- genderLeadership.js
+++ genderLeadership.js
@@ -84,7 +84,7 @@ define(['pipAPI','https://denisakov.github.io/qualtrics/iat.js'], function(APICo
 		//In Project Implicit's website, you can see that we added much text to explain that there is still much unknown about the meaning of these results.
 		//We strongly recommend that you provide all these details in the debriefing of the experiment.
 		debriefingTextBottom : 'This result is not a definitive assessment of your attitudes. It is provided for educational purposes only.', //Will be shown below the feedback text.
-
+		debriefingTextBottomTouch : 'Lutfen sonucunuzu aklinizda tutunuz ve bir sonraki asamada coktan secmeli menuden seciniz.' //Will be shown below the feedback text.
 
 		// preDebriefingText : 'Asagidaki yesil alana dokunarak sonucunuza ulasabilirsiniz.', //Text in the trial that comes before showing the debriefing.
 		// preDebriefingTouchText : 'Asagidaki yesil alana dokunarak sonucunuza ulasabilirsiniz.', //Touch version for the text in the trial that comes before showing the debriefing.
